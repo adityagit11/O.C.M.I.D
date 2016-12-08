e444d8e55aae3bf3a6717f0c60116d7a063b9a86
swapnil-ss <swapnil31kgp@gmail.com>
2016-12-08 21:37:51 +0530

    UPdating files

--- OCMID.py
+++ OCMID.py
@@ -1,25 +1,44 @@
-#V1.0 - 26/07/2016
+from flask import Flask, jsonify, send_file
+from flask import abort
+from flask import make_response
+from flask import request
+from flask import url_for
+from PIL import Image
+import csv
+import httplib
+from httplib import HTTP
+from urlparse import urlparse
+import urllib2
+import cStringIO
+import urllib
+import os
+import time
+from urlparse import urlparse
+from threading import Thread
+import httplib, sys, getopt
+from Queue import Queue
+import StringIO
 
-#importing the urlib.request module for the interfacing of the program from the internet.
-import urllib.request;
-print("hello world");
-#making a file object named as fileOpenObjectOnReadMode for the file data.txt in the read mode.
-fileOpenObjectOnReadMode = open("data.txt","r");
-#we imported the os module for the directory creation at the os.path
-#or simply wherever the wolvieDummy.py/file is saved as!.
-import os;
-#os.makedirs() method is used for the creation of the directory named as the DownloadedImageURLFile.
-os.makedirs("DownloadedImageURLFile");
-#creation of a integer implicitly declared variable named as indexForName.
-indexForName = 0;
-#using the for loop and iterate through every URL present in the file data.txt via the file object : fileOpenObjectOnReadMode
-for lineByLineIterate in fileOpenObjectOnReadMode:
-    #using the try - except clause.
-    try:
-        urllib.request.urlretrieve(lineByLineIterate,"DownloadedImageURLFile/"+str(indexForName)+".jpeg");
-        indexForName = indexForName + 1;
-    #if you see carefully we are catching every exception produced over here
-    except:
-        print("ExceptionHTTP 403 Error @ : "+str(indexForName));
-        continue;
-#hence the version is completed
+def check_url(url):
+	try:
+		connection = urllib2.urlopen(url)
+		return connection.getcode()
+		connection.close()
+	except:
+		return 404
+
+with open('/home/swapnil/Desktop/O.C.M.I.D/data.txt', 'r') as f :
+    x = 0
+    for line in f :
+       url = line
+       if (check_url(line)<400):
+           file = urllib.urlopen(url)
+           img = Image.open(file)
+           newpath = '/home/swapnil/Desktop/O.C.M.I.D/images/' 
+           if not os.path.exists(newpath):
+                os.makedirs(newpath)
+           img.save("/home/swapnil/Desktop/O.C.M.I.D/images/"+str(x+1)+'.jpg', img.format, quality=100)
+           x = x+1
+       else :
+           x = x
+            
